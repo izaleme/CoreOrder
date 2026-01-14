24b737ffa8c0ad452053a127f45f4c1981bdd917
Izabela Leme <izaleme00@gmail.com>
2026-01-14 15:48:40 -0300

    AuthService base files

--- src/AuthService/AuthService.API/Program.cs
+++ src/AuthService/AuthService.API/Program.cs
@@ -1,48 +1,26 @@
 using System.Reflection;
 using System.Text;
 
-//try
-//{
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    // Services
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    // Pipeline
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+// Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
-    app.MapControllers();
-    app.MapGet("/", () => "Auth Service is running!");
+app.MapControllers();
+app.MapGet("/", () => "Auth Service is running!");
 
-    app.Run();
-//}
-//catch (ReflectionTypeLoadException ex)
-//{
-//    StringBuilder sb = new StringBuilder();
-//    foreach (Exception exSub in ex.LoaderExceptions)
-//    {
-//        sb.AppendLine(exSub.Message);
-//        FileNotFoundException exFileNotFound = exSub as FileNotFoundException;
-//        if (exFileNotFound != null)
-//        {
-//            if (!string.IsNullOrEmpty(exFileNotFound.FusionLog))
-//            {
-//                sb.AppendLine("Fusion Log:");
-//                sb.AppendLine(exFileNotFound.FusionLog);
-//            }
-//        }
-//        sb.AppendLine();
-//    }
-//    string errorMessage = sb.ToString();
-//}
+app.Run();
